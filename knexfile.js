857475af61918d49b8bc7df0df15dd786c7c37bd
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 11:16:17 -0400

    Setup testing and development db env

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/testing.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+};
