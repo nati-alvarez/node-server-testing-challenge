6b67d6b1abd3edf3e613adaf468e985cced59d3a
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 11:33:51 -0400

    Wrote test to check type of data returned from pet-stores rotue

--- server.spec.js
+++ server.spec.js
@@ -5,6 +5,12 @@ describe("server.js", ()=>{
     describe("pet-stores route", ()=>{
         it("Should respond with 200 status code", async()=>{
             const response = await request(server).get("/pet-stores");
+            expect(response.status).toEqual(200);
+        });
+
+        it("Should return an array of pet stores", async()=>{
+            const response = await request(server).get("/pet-stores");
+            expect(Array.isArray(response.body)).toEqual(true);
         });
     });
 });
