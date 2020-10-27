f621043fdcf5da80966ef4cd61b00305bd3db72c
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 12:25:15 -0400

    Added to tests for petStoresModel

--- pet_stores/petStoresModel.spec.js
+++ pet_stores/petStoresModel.spec.js
@@ -2,10 +2,28 @@ const db = require("../data/db");
 const petStoresModel = require("./petStoresModel");
 
 describe("pet stores model", ()=>{
+    beforeEach(async()=>{
+        await db("Pet_Store").truncate();
+    });
+
     describe("getAll", ()=>{
         it("Should return an array of pet stores", async()=>{
             const stores = await petStoresModel.getAll();
             expect(Array.isArray(stores)).toEqual(true);
         });
-    })
+
+        it("Should contain objects representing pet stores", async()=>{
+            const store = {id: 1, name: "Petco", address: "123 Place Ln", specialty: "Pet grooming"};
+            await db("Pet_Store").insert(store);
+            const stores = await petStoresModel.getAll();
+            expect(stores[0]).toEqual(store);
+        });
+
+        it("Should have length of 1", async()=>{
+            const store = {id: 1, name: "Petco", address: "123 Place Ln", specialty: "Pet grooming"};
+            await db("Pet_Store").insert(store);
+            const stores = await petStoresModel.getAll();
+            expect(stores.length).toEqual(1);
+        })
+    });
 })
