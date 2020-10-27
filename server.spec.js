46f0d408c0752f12769cb336e5069f729ff67343
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 14:43:19 -0400

    Wrote tests for DELETE /pet-stores/:id endpoint

--- server.spec.js
+++ server.spec.js
@@ -51,5 +51,49 @@ describe("server.js", ()=>{
 
             expect(response.body).toEqual(body);
         });
+    });
+
+    describe("pet-stores DELETE route", ()=>{
+        beforeEach(async()=>{
+            await db("Pet_Store").truncate();
+            await db("Pet_Store").insert({id: 1, name: "Petco", address: "123 Place Ln", specialty: "Pet grooming"});
+        });
+
+        it("Should respond with 204 status code on successful delete", async()=>{
+            const response = await request(server).delete("/pet-stores/1");
+
+            expect(response.status).toEqual(204);
+        });
+
+        it("Should return success message on successful delete", async()=>{
+            const expectedResponse = {message: "Deleted successfully"};
+            const response = await request(server).delete("/pet-stores/1");
+
+            expect(response.body).toEqual(expectedResponse);
+        });
+
+        it("Should return 400 status code with error message if not given valid id", async()=>{
+            //valid ids are integers only
+            //strings not coercible to integers and floating point numbers should get this response
+            //strings like '4', '2342' are allowed
+            const expectedResponse = {message: "Store id not valid"};
+            const response = await request(server).delete("/pet-stores/petco");
+
+            expect(response.status).toEqual(400);
+            expect(response.body).toEqual(expectedResponse);
+        });
+
+        it("Should respond with 404 status code if delete tried on nonexistent store", async()=>{
+            const response = await request(server).delete("/pet-stores/38992");
+
+            expect(response.status).toEqual(404);
+        });
+
+        it("Should return appropriate error message if delete triend on nonexistent store", async()=>{
+            const expectedResponse = {message: "Store does not exist"};
+            const response = await request(server).delete("/pet-stores/2340383");
+
+            expect(response.body).toEqual(expectedResponse);
+        });
     })
 });
