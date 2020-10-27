bbee748f68e859fb43564db661a56b97be67eddd
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 12:51:35 -0400

    addStore method passing all tests

--- pet_stores/petStoresModel.js
+++ pet_stores/petStoresModel.js
@@ -1,9 +1,17 @@
 const db = require("../data/db");
 
 module.exports = {
-    getAll
+    getAll,
+    addStore
 };
 
 function getAll(){
     return db("Pet_Store");
+}
+
+async function addStore(store){
+    const insertion = await db("Pet_Store").insert(store);
+    const newStoreId = insertion[0];
+
+    return db("Pet_Store").where({id: newStoreId}).first();
 }
