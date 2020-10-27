4b5abf255256e9583f6c5e6b547d953de924f531
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 14:09:51 -0400

    Wrote tests for POSt /pet-stores endpoint

--- server.spec.js
+++ server.spec.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const server = require("./server.js");
+const db = require("./data/db");
 
 describe("server.js", ()=>{
     describe("pet-stores GET route", ()=>{
@@ -18,4 +19,37 @@ describe("server.js", ()=>{
             expect(response.type).toEqual("application/json");
         })
     });
+    
+    describe("pet-stores POST route", ()=>{
+        beforeEach(async()=>{
+            await db("Pet_Store").truncate();
+        });
+
+        it("Should respond with 201 status code on successful insertion", async()=>{
+            const body = {id: 1, name: "Petco", address: '123 Place Ln', specialty: "Pet grooming"};
+            const response = await request(server).post("/pet-stores").send(body);
+
+            expect(response.status).toEqual(201);
+        });
+
+        it("Should respond with 400 status if required data not given", async()=>{
+            const response = await request(server).post("/pet-stores").send({});
+
+            expect(response.status).toEqual(400);
+        });
+
+        it("Should return relevant error message if required data not given", async()=>{
+            const errorResponse = {message: "Store name and address required"};
+            const response = await request(server).post("/pet-stores").send({});
+
+            expect(response.body).toEqual(errorResponse);
+        });
+
+        it("Should return the new store on successful creation", async()=>{
+            const body = {id: 1, name: "Petco", address: '123 Place Ln', specialty: "Pet grooming"};
+            const response = await request(server).post("/pet-stores").send(body);
+
+            expect(response.body).toEqual(body);
+        });
+    })
 });
