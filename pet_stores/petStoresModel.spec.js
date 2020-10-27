8614bdd8c945160da64d985d562c1a8d530acecb
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-27 15:04:03 -0400

    Wrote tests for petStoresModel

--- pet_stores/petStoresModel.spec.js
+++ pet_stores/petStoresModel.spec.js
@@ -47,5 +47,35 @@ describe("pet stores model", ()=>{
             const store = await petStoresModel.addStore(newStore);
             expect(store).toEqual(newStore);
         });
-    })
-})
+    });
+    describe('removeStore', ()=>{
+        beforeEach(async()=>{
+            const store1 = {id: 1, name: "Petco", address: '123 Place Ln', specialty: "Pet grooming"};
+            const store2 = {id: 2, name: "Petsense", address: "678 Pet Place", specialty: "Small pets"};
+            const store3 = {id: 3, name: "PetSmart", address: "934 Street St", specialty: "Pet food"};
+            await db("Pet_Store").insert([store1, store2, store3]);
+        });
+
+        it("Should remove only one record from db", async()=>{
+            await petStoresModel.removeStore(1);
+            const stores = await db("Pet_Store");
+
+            expect(stores).toHaveLength(2);
+        });
+
+        it("Should remove the specified store from db", async()=>{
+            await petStoresModel.removeStore(1);
+            const store = await db("Pet_Store").where({id: 1}).first();
+            
+            expect(store).toEqual(undefined);
+        });
+
+        it("Should return store that was removed", async()=>{
+            const expectedStore = {id: 1, name: "Petco", address: '123 Place Ln', specialty: "Pet grooming"};
+            const removedStore = petStoresModel.removeStore(1);
+            
+            expect(removedStore).toEqual(expectedStore);
+        });
+    });
+
+});
